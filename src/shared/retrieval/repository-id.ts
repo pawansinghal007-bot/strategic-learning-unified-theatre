7345c1b204e1d77e913066ff8b26062eea1d9029
Pawan <pawan@localhost>
2026-07-11 23:20:59 +0530

    fix(sonar): S3790 in repository-id.ts:25 — replace parseInt with Number.parseInt

--- src/shared/retrieval/repository-id.ts
+++ src/shared/retrieval/repository-id.ts
@@ -22,7 +22,7 @@ export function getRepositoryId(): string {
     hash.slice(0, 8),
     hash.slice(8, 12),
     "5" + hash.slice(13, 16), // version 5
-    ((parseInt(hash.slice(16, 18), 16) & 0x3f) | 0x80).toString(16) + hash.slice(18, 20),
+    ((Number.parseInt(hash.slice(16, 18), 16) & 0x3f) | 0x80).toString(16) + hash.slice(18, 20),
     hash.slice(20, 32),
   ].join("-");
 }
